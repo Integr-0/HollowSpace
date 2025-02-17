f6263f18b7bf843df4d2228c266a9edbca0b2033
luckylukii <lukas.gruenzweil@liwest.at>
2025-02-17 19:20:19 +0100

    Enemy Gizmos

---
+++ HollowSpace/Assets/Ideas.md
@@ -0,0 +1,19 @@
+# Game Mechanics to prototype
+
+- **Darkness Pool**
+  - Swallows Lights
+    - Decrease intensity of all lights inside
+  - Slows down player
+  - Damage over time
+
+- **Pushable Boxes**
+
+- **Cables that electricity can travel through**
+  - Various energy source (Light, Batteries, Buttons)
+  - Charged cables emmit light
+
+<style>
+* {
+    margin-top: 5px;
+}
+</style>
--- HollowSpace/Assets/_Project/_Scripts/Enemy/EnemyPathfind.cs
+++ HollowSpace/Assets/_Project/_Scripts/Enemy/EnemyPathfind.cs
@@ -3,6 +3,23 @@ using System.Collections.Generic;
 
 [RequireComponent(typeof(Enemy))]
 public class EnemyPathfind : MonoBehaviour {
+    
+    private class Node {
+        public Vector2 Position;
+        public Node Parent;
+        public float G; // Cost from start to current node
+        public float H; // Heuristic cost from current node to goal
+        public float F; // Total cost (G + H)
+
+        public Node(Vector2 position, Node parent, float g, float h) {
+            Position = position;
+            Parent = parent;
+            G = g;
+            H = h;
+            F = g + h;
+        }
+    }
+    
     [SerializeField] private Transform target;
     [SerializeField] private LayerMask obstacleLayer;
     [SerializeField] private float stepSize = 0.5f;
@@ -13,6 +30,10 @@ public class EnemyPathfind : MonoBehaviour {
     private List<Vector2> _path;
     private int _currentPathIndex;
     private Vector2 _lastTargetPosition;
+    
+    // Pathfinding data for Gizmos
+    private List<Node> _openSet;
+    private HashSet<Vector2> _closedSet;
 
     private void Start() {
         _enemy = GetComponent<Enemy>();
@@ -75,19 +96,19 @@ public class EnemyPathfind : MonoBehaviour {
         start = RoundToNearestStep(start);
         goal = RoundToNearestStep(goal);
 
-        var openSet = new List<Node> { new Node(start, null, 0, Vector2.Distance(start, goal)) };
-        var closedSet = new HashSet<Vector2>();
+       _openSet = new List<Node> { new Node(start, null, 0, Vector2.Distance(start, goal)) };
+        _closedSet = new HashSet<Vector2>();
         int iterations = 0;
 
-        while (openSet.Count > 0) {
+        while (_openSet.Count > 0) {
             if (iterations++ > maxIterations) {
                 Debug.LogWarning("Pathfinding exceeded maximum iterations.");
                 break;
             }
 
-            openSet.Sort((a, b) => a.F.CompareTo(b.F));
-            var currentNode = openSet[0];
-            openSet.RemoveAt(0);
+            _openSet.Sort((a, b) => a.F.CompareTo(b.F));
+            var currentNode = _openSet[0];
+            _openSet.RemoveAt(0);
 
             if (currentNode.Position == goal) {
                 var path = new List<Vector2>();
@@ -100,16 +121,16 @@ public class EnemyPathfind : MonoBehaviour {
                 return path;
             }
 
-            closedSet.Add(currentNode.Position);
+            _closedSet.Add(currentNode.Position);
 
             foreach (var neighbor in GetNeighbors(currentNode.Position)) {
-                if (closedSet.Contains(neighbor) || IsIlluminated(neighbor)) continue;
+                if (_closedSet.Contains(neighbor) || IsIlluminated(neighbor)) continue;
 
                 var tentativeG = currentNode.G + Vector2.Distance(currentNode.Position, neighbor);
-                var existingNode = openSet.Find(n => n.Position == neighbor);
+                var existingNode = _openSet.Find(n => n.Position == neighbor);
 
                 if (existingNode == null) {
-                    openSet.Add(new Node(neighbor, currentNode, tentativeG, Vector2.Distance(neighbor, goal)));
+                    _openSet.Add(new Node(neighbor, currentNode, tentativeG, Vector2.Distance(neighbor, goal)));
                 }
                 else if (tentativeG < existingNode.G) {
                     existingNode.Parent = currentNode;
@@ -149,19 +170,35 @@ public class EnemyPathfind : MonoBehaviour {
                _enemy.stats.attackRange * _enemy.stats.attackRange;
     }
 
-    private class Node {
-        public Vector2 Position;
-        public Node Parent;
-        public float G; // Cost from start to current node
-        public float H; // Heuristic cost from current node to goal
-        public float F; // Total cost (G + H)
-
-        public Node(Vector2 position, Node parent, float g, float h) {
-            Position = position;
-            Parent = parent;
-            G = g;
-            H = h;
-            F = g + h;
+    
+    private void OnDrawGizmosSelected() {
+        if (!Application.isPlaying) return;
+        
+        // Attacking
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _enemy.stats.attackRange);
+        
+        // Pathfinding
+        if (_path.Count > 0) {
+            Gizmos.color = Color.green;
+            for (int i = 0; i < _path.Count - 1; i++) {
+                Gizmos.DrawLine(_path[i], _path[i + 1]);
+            }
+        }
+        
+        // Pathfinding data
+        if (_openSet != null) {
+            Gizmos.color = Color.blue;
+            foreach (var node in _openSet) {
+                Gizmos.DrawWireSphere(node.Position, 0.1f);
+            }
+        }
+        
+        if (_closedSet != null) {
+            Gizmos.color = Color.red;
+            foreach (var position in _closedSet) {
+                Gizmos.DrawWireSphere(position, 0.1f);
+            }
         }
     }
 }
